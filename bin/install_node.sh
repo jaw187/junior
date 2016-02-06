e2400512654c9dbc0bc69304f8296176f471aaa6
James Weston <jawone87@gmail.com>
2016-02-05 22:33:26 -0500

    FIRST

---
+++ bin/install_node.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+NODE_VERSION=$1
+
+PATH=/node/bin:${PATH}
+CURRENT=$(/node/bin/node --version)
+if [ "${CURRENT}" == "${NODE_VERSION}" ];
+then
+    echo "Already at version ${NODE_VERSION}"
+else
+    cd /
+    rm -rf node*
+    curl -s -O "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz"
+    tar xf node-v${NODE_VERSION}-linux-x64.tar.xz
+    mv node-v${NODE_VERSION}-linux-x64 node
+
+    NEW_VERSION=$(/node/bin/node --version)
+    if [ "${NEW_VERSION}" != "${NODE_VERSION}" ];
+    then
+        echo "${NEW_VERSION} did not match ${NODE_VERSION}"
+        exit 1
+    fi
+fi
+
+## install global npm
+for PACKAGE in npm
+do
+   LATEST=$(/node/bin/npm show ${PACKAGE} version | head -1)
+   CURRENT=$(${PACKAGE} --version)
+   if [ "${LATEST}" != "${CURRENT}" ];
+   then
+      echo "Upgrading ${PACKAGE} from version ${CURRENT} to version ${LATEST}"
+      # npm attempts to write to /tmp
+      # sudo chmod 1777 /tmp
+      /node/bin/npm -g install ${PACKAGE}
+   else
+      echo "${PACKAGE} already at latest version ${LATEST}"
+   fi
+done
